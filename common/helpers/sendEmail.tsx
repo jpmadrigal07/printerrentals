9ba730206d03f581342e90e2d88e90344347e399
JP Madrigal <jp.madrigal07@gmail.com>
2024-08-08 09:16:08 +0800

    refactor: Update "postmark" import in sendEmail helper

--- common/helpers/sendEmail.tsx
+++ common/helpers/sendEmail.tsx
@@ -1,5 +1,5 @@
 import { render } from '@react-email/components';
-import postmark from "postmark";
+const postmark = require("postmark");
 import { EmailTemplate } from "@/common/components/Email/EmailTemplate";
 
 const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY || '');
