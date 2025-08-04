2d59010b22db90ece8af3ffa765bd72207820013
JP Madrigal <jp.madrigal07@gmail.com>
2025-08-04 19:01:39 +0800

    feat: Remove Postmark API key and ensure SMTP configuration is set up correctly

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,12 @@ const nextConfig = {
     RECAPTCHA_SECRET: process.env.RECAPTCHA_SECRET,
     RECAPTCHA_PUBLIC: process.env.RECAPTCHA_PUBLIC,
     EMAIL_RECEIVER: process.env.EMAIL_RECEIVER,
-    POSTMARK_API_KEY: process.env.POSTMARK_API_KEY,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_SECURE: process.env.SMTP_SECURE,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    SMTP_FROM: process.env.SMTP_FROM,
   },
 };
 
